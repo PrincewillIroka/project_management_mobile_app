aea0ecd2554d3c0567454bf46035bfd892249a74
Princewill Iroka <princewilliroka@outlook.com>
2021-06-23 19:17:22 +0100

    Implemented SignUp screen

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,10 +1,91 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+  Switch,
+} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Octicons from 'react-native-vector-icons/Octicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import styles from './signUpStyle';
+import {navigateToNestedRoute} from '../../navigators/RootNavigation';
+import {getScreenParent} from '../../utils/NavigationHelper';
+import appTheme from '../../constants/colors';
 
-export function SignUp() {
+export function SignUp({navigation}) {
+  const handleBackButton = () => {
+    navigation?.goBack();
+  };
+
+  const handleNavigation = (screen, params) => {
+    navigateToNestedRoute(getScreenParent(screen), screen, params);
+  };
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => handleBackButton()}>
+          <MaterialIcons name="keyboard-arrow-left" size={25} color="gray" />
+          <Text style={styles.backText}>Back</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bodyContent}>
+        <Text style={styles.largeText}>Welcome Back!</Text>
+        <Text style={styles.smallText}>
+          Log into your account &amp; manage {'\n'}your tasks
+        </Text>
+        <View style={styles.inputRow}>
+          <Ionicons name="person-outline" size={20} color="gray" />
+          <TextInput
+            placeholder="Username"
+            placeholderTextColor="gray"
+            style={styles.textInput}
+          />
+        </View>
+        <View style={styles.inputRow}>
+          <MaterialCommunityIcons name="email-outline" size={20} color="gray" />
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor="gray"
+            secureTextEntry={true}
+            style={styles.textInput}
+          />
+        </View>
+        <View style={styles.inputRow}>
+          <MaterialIcons name="lock-outline" size={20} color="gray" />
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor="gray"
+            secureTextEntry={true}
+            style={styles.textInput}
+          />
+          <Octicons name="eye-closed" size={20} color="gray" />
+        </View>
+        <View style={styles.savePwdRow}>
+          <Text style={styles.savePwdText}>Save Password</Text>
+          <Switch
+            trackColor={{false: appTheme.INACTIVE_COLOR, true: appTheme.COLOR2}}
+            thumbColor="#fff"
+            value={true}
+          />
+        </View>
+        <TouchableOpacity style={styles.signUpBtnWrapper}>
+          <Text style={styles.signUpBtnText}>SIGN UP</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.loginBtnWrapper}
+          onPress={() => handleNavigation('Login')}>
+          <Text style={styles.loginBtnText}>
+            Already have an account? LOGIN
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
